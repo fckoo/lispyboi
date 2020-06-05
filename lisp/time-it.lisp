25a6201a4acd42e92de1f3ae707c67562e9a8375
Max Mickey <max.mickey1991@gmail.com>
2020-06-05 15:37:54 -0500

    helper for timing/benchmarking

---
+++ lisp/time-it.lisp
@@ -0,0 +1,9 @@
+(defmacro time-it (time-var expr &body body)
+  (let ((start-sym (gensym "TIME-START")))
+    `(let ((,start-sym (get-clock-ticks)))
+       ,expr
+       (let ((,time-var (%/ (- (get-clock-ticks) ,start-sym)
+                            1000)))
+         ,@body))))
+
+(provide "time-it")
