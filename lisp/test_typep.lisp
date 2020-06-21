34d015854858682793fb7a97ed15b806e3ce2f46
Max Mickey <max.mickey1991@gmail.com>
2020-06-21 07:59:00 -0500

    add typep tests

---
+++ lisp/test_typep.lisp
@@ -0,0 +1,21 @@
+(require "asserts")
+(require "typep")
+
+
+(assert-true (typep #\A 'character))
+(assert-true (typep 123 'fixnum))
+(assert-true (typep "test" 'string))
+(assert-true (typep "test" 'array))
+(assert-true (typep "test" '(simple-array)))
+(assert-true (typep "test" '(simple-array character)))
+(assert-true (typep "test" '(simple-array character 4)))
+(assert-true (typep "test" '(simple-array * 4)))
+(assert-true (typep "test" '(simple-array * *)))
+
+(assert-false (typep "test" '(simple-array character 5)))
+(assert-false (typep "test" '(simple-array * 3)))
+
+(assert-true (typep (list 1 2 3) 'cons))
+(assert-true (typep (list 1 2 3) 'list))
+(assert-true (typep nil 'list))
+(assert-false (typep nil 'cons))
