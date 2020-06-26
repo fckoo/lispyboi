610dcc5dc28caada8f2a1a70fb7fc129955ea023
Max Mickey <max.mickey1991@gmail.com>
2020-06-26 06:36:40 -0500

    change format to use print-object and not allocate so many intermediate strings

--- lib/asserts.lisp
+++ lib/asserts.lisp
@@ -6,8 +6,7 @@
        (if ,tmp-var-name
            'pass
            (progn
-             (print "Assertion failed for ")
-             (print-line ',expr)
+             (format t "Assertion failed for ~s~%" ',expr)
              'fail)))))
 
 (defmacro assert-false (expr)
@@ -15,8 +14,7 @@
     `(let ((,tmp-var-name ,expr))
        (if ,tmp-var-name
            (progn
-             (print "Assertion failed for ")
-             (print-line ',expr)
+             (format t "Assertion failed for ~s~%" ',expr)
              'fail)
            'pass))))
 
@@ -28,14 +26,8 @@
        (if (,test ,actual-var-name ,expected-var-name)
            'pass
            (progn
-             (print ',test)
-             (print " assertion failed for ")
-             (print-line ',actual)
-             (print "  Expected to get ")
-             (print ,expected-var-name)
-             (print " but got ")
-             (print ,actual-var-name)
-             (print-line " instead.")
+             (format t "~a assertion failed for ~a~%" ,(format nil "~s" test) ,(format nil "~s" actual))
+             (format t "  Expected to get ~s but got ~s instead.~%" ,expected-var-name ,actual-var-name)
              'fail)))))
 
 (defmacro assert-eq (expected actual)
--- lisp/tests/test_defgeneric.lisp
+++ lisp/tests/test_defgeneric.lisp
@@ -11,7 +11,7 @@
 
 
 (assert-string= "fixnum 123" (test-generic 123))
-(assert-string= "character #\\\\A" (test-generic #\A))
+(assert-string= "character #\\A" (test-generic #\A))
 (assert-string= "string \"hello world\"" (test-generic "hello world"))
 (assert-string= "symbol FOO" (test-generic 'foo))
 (assert-string= "default (1 2 3)" (test-generic '(1 2 3)))
@@ -19,11 +19,11 @@
 
 ;; test automatic defgeneric when there was no prior defgeneric for the function
 (defmethod this-method-does-not-exist ((a character) b)
-  (code-char (+ (char-code a) b)))
+  "character spec")
 
 (defmethod this-method-does-not-exist ((a fixnum) b)
-  (code-char (+ a b)))
+  "fixnum spec")
 
 
-(assert-eql #\K (this-method-does-not-exist #\A 10))
-(assert-eql #\K (this-method-does-not-exist 65 10))
+(assert-string= "character spec" (this-method-does-not-exist #\A 10))
+(assert-string= "fixnum spec" (this-method-does-not-exist 65 11))
