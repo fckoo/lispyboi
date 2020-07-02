5ce389da9ce20df728998fc96ba5b00e91da4990
Max Mickey <max.mickey1991@gmail.com>
2020-07-02 13:51:36 -0500

    change SYMBOL-MACROLET to use %LEXICAL-WALK-REPLACE

--- lib/symbol-macrolet.lisp
+++ lib/symbol-macrolet.lisp
@@ -1,16 +1,6 @@
 (provide "symbol-macrolet")
 
 (defmacro symbol-macrolet (macro-bindings &body body)
-  (labels ((macrolet-transform (expr)
-             (typecase expr
-               (symbol (let ((found (assoc expr macro-bindings)))
-                         (if found
-                             (second found)
-                             expr)))
-               (cons
-                (case (car expr)
-                  (quote expr)
-                  (lambda `(lambda ,(second expr) ,@(macrolet-transform (cddr expr))))
-                  (t (map1 #'macrolet-transform expr))))
-               (t expr))))
-    `(progn ,@(macrolet-transform body))))
+  `(progn ,@(%lexical-walk-replace body
+                                   (lambda (e)
+                                     (second (assoc e macro-bindings))))))
