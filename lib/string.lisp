3ace2a9a0f43661a102b04f8a396ea51b726fdf0
Max Mickey <max.mickey1991@gmail.com>
2020-07-31 20:00:02 -0500

    add STRING-TRIM and STRING-SPLIT now accept multi-character delimiter

--- lib/string.lisp
+++ lib/string.lisp
@@ -1,17 +1,37 @@
 (in-package :lispyboi)
 (provide "string")
 
+
+(defun string-match (string index match-string)
+  (when (<= (+ index (length match-string)) (length string))
+    (let ((i 0))
+      (while (and (< i (length match-string))
+                  (eql (aref string (+ index i)) (aref match-string i)))
+             (incf i))
+      (= i (length match-string)))))
+
+
+
+(defun delimiter-p (string index delimiter)
+  (typecase delimiter
+    (character (and (< index (length string))
+                    (eql delimiter (aref string index))))
+    (string (string-match string index delimiter))))
+
 (defun string-split (string &optional (delimiter #\Space) max-splits)
   (let ((splits)
         (start 0)
-        (end 0))
+        (end 0)
+        (delim-length (typecase delimiter
+                        (character 1)
+                        (string (length delimiter)))))
     (unless max-splits
       (setf max-splits -1))
     (while (and (/= 0 max-splits)
                 (< end (length string)))
-           (when (eql delimiter (aref string end))
+           (when (delimiter-p string end delimiter)
              (push (substring string start end) splits)
-             (setf start (+ 1 end))
+             (setf start (+ delim-length end))
              (decf max-splits))
            (incf end))
     (when (< start end)
@@ -20,6 +40,19 @@
                (< end (length string)))
       (push (substring string end) splits))
     (reverse! splits)))
+
+(defun string-trim (string)
+  (let ((start 0)
+        (end (length string)))
+    (while (and (< start end)
+                (spacep (aref string start)))
+           (incf start))
+    (while (and (> end start)
+                (spacep (aref string (- end 1))))
+           (decf end))
+    (if (= start end)
+        (make-string)
+        (substring string start end))))
 
 (defun char-downcase (c)
   (if (<= (char-code #\A) (char-code c) (char-code #\Z))
@@ -46,6 +79,7 @@
   string)
 
 (export '(string-split
+          string-trim
           char-downcase
           char-upcase
           string-upcase!
